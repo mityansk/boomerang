42b74043e70d0c9a58cabdd3d45a5a3afb7a514f
mityansk <retest@gmail.com>
2025-01-17 11:17:46 +0300

    hotfix

--- src/View.js
+++ src/View.js
@@ -1,12 +1,10 @@
-// Сделаем отдельный класс для отображения игры в консоли.
-
 class View {
-  render() {
+  render(track) {
     const yourTeamName = 'Mighty Mushrooms';
 
     // Тут всё рисуем.
     console.clear();
-    console.log(this.game.track.join(''));
+    console.log(track.join(''));
     console.log('\n\n');
     console.log(`Created by "${yourTeamName}" with love`);
   }
