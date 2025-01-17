6d59d6d7b16a9611e3f979d1840c6fe57999f936
Sensdoo <sensdoo.dev@gmail.com>
2025-01-17 16:13:35 +0300

    added addition userok to db

--- src/Game.js
+++ src/Game.js
@@ -6,6 +6,8 @@ const Hero = require('./game-models/Hero');
 const Enemy = require('./game-models/Enemy');
 const Boomerang = require('./game-models/Boomerang');
 const View = require('./View');
+const Userok = require('./User');
+const { User } = require('./db/models');
 const keyboard = require('./keyboard');
 
 // Основной класс игры.
@@ -19,6 +21,9 @@ class Game {
     this.boomerang = new Boomerang(this.hero, this.enemy);
     this.view = new View();
     this.track = Array(trackLength).fill(' ');
+    this.username = this.view.getUserName();
+    this.user = new Userok(this.username, `${this.username}@megagame.com`);
+    User.create(this.user);
     keyboard(this.boomerang);
     this.regenerateTrack();
   }
@@ -38,6 +43,9 @@ class Game {
     }
     if (this.boomerang.state === 'hidden') {
       this.track[this.boomerang.position + 1] = ' ';
+    }
+    if (this.boomerang.state === 'returned') {
+      this.view.gameOver(this.username);
     }
   }
 
