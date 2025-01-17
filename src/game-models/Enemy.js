20ffff74738d6ee6bda244d89c7409ba02f59afd
Sensdoo <sensdoo.dev@gmail.com>
2025-01-17 13:58:10 +0300

    impl regenerateTack function, v:0.1

--- src/game-models/Enemy.js
+++ src/game-models/Enemy.js
@@ -3,11 +3,24 @@
 class Enemy {
   constructor() {
     this.generateSkin();
-    this.position = 2;
+    this.position = 30;
   }
 
   generateSkin() {
-    const skins = ['👾', '💀', '👹', '👻', '👽', '👿', '💩', '🤡', '🤺', '🧛', '🧟', '🎃'];
+    const skins = [
+      '👾',
+      '💀',
+      '👹',
+      '👻',
+      '👽',
+      '👿',
+      '💩',
+      '🤡',
+      '🤺',
+      '🧛',
+      '🧟',
+      '🎃',
+    ];
     this.skin = skins[Math.floor(Math.random() * skins.length)];
   }
 
